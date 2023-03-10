0650e1a98c2264b3d558e3402423d7dc3a138991
unknown <icallewaert@gamingcampus.local>
2023-03-10 11:15:36 +0100

    add icon change

---
+++ Assets/Scripts/ChangeIcon.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeIcon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DisableIcon()
+    {
+        gameObject.SetActive(false);
+    }
+    public void EnableIcon()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void ChangeIconTexture(Sprite sprite)
+    {
+        Debug.Log(GetComponent<Image>().mainTexture);
+        GetComponent<Image>().sprite = sprite;
+    }
+}
